d3423943f458dc06a202397cd8825277737823d8
Yana-Purik <Def4onkA-YaNa@yandex.ru>
2012-12-26 17:30:00 +0400

    Update k01-362-12-2.sh

--- k01-362-12-2.sh
+++ k01-362-12-2.sh
@@ -1,38 +1,63 @@
+
 #!/bin/bash
 
 echo "Программа поиска файлов по имени или размеру"
 echo "Разработчик: Пурик Яна"
 
-
-while : 
+while [ true ] 
 do
-echo "Выберите действие: 1.Поиск по имени файла 2.Поиск по размеру"
-read d
-echo "Введите каталог, в котором следует осуществлять поиск"
-read c
+echo "Начать выполнение?(y/n)"
+read ex
+ case $ex in
+  n )  
+     echo "Программа завершена" 
+     exit 0
+  ;;
+  y ) 
+   while [ $ex = y  ]; do
+   echo "Выберите действие: 1.Поиск по имени файла 2.Поиск по размеру"
+   read d 
+   case $d in
+    1 )
+   echo -n "введите католог для поиска "
+   read c
+       while [ ! -d "$c" ]; do 
+       echo -n "каталога не существует! Еще раз?(y/n) "
+       read ex
+        case $ex in 
+        y ) 
+          echo -n "Введите каталог ";
+          read c
+        ;;
+        n )
+        exit 0
+        ;;
+        esac
+       done
+       echo "Введите имя файла"
+       read name
+       test -f "$c*$name*.*"     
+         find $c -type f -name *$name*.* -print  2>/dev/null
+          ex=n       
+        ;;
 
-case  $d in 
+        2 )
+         echo "введите размер в килобайтах"
+         read s
+         echo $s
+         find $c -size +"$s"k 2>/dev/null
+         break
+        ;;
 
-1 ) 
-echo "Введите имя файла"
-read n
-find $c -type f -name *$n*.* 
-break
-;;
+        * )
+         echo "Ошибка!!! хотите продолжить?(y\n)"
+         read ex
+         if [  $ex = n ]; then 
+           exit 0
+           fi
+        esac
+     done
+  esac
 
-2 )
-echo "введите размер в килобайтах"
-read s
-echo $s
-find $c -size +"$s"k
-break
-;;
-
-*)
-echo "Ошибка!!! хотите продолжить?(y\n)"
-read cont
-if [ $cont != y ]
-then exit
-fi
-esac 
-done 
+done
+exit 0
